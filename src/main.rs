f5216910b33d1e4b0fe5f9fa35abb0a850d2d4c1
Sam Yaple <sam@yaple.net>
2016-11-05 19:00:28 +0000

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+#![cfg_attr(feature = "serde_derive", feature(proc_macro))]
+
+#[cfg(feature = "serde_derive")]
+#[macro_use]
+extern crate serde_derive;
+
+extern crate serde;
+extern crate serde_json;
+
+#[cfg(feature = "serde_derive")]
+include!("main.rs.in");
+
+#[cfg(not(feature = "serde_derive"))]
+include!(concat!(env!("OUT_DIR"), "/main.rs"));
